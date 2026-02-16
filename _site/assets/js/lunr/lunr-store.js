a4c67080f8a4a412821711608516a9fcd18ae3f5
Tsung-Han Yang <tsung-han_yang@alumni.brown.edu>
2026-02-16 15:49:03 -0500

    .

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "How a short-range disorder changes the bulk properties?",
+        "excerpt":"Introduction  Topological properties rely on the symmetry of electronic and magnetic degrees of freedom.   References   ","categories": ["Quantum Materials","Data Science"],
+        "tags": ["topology","quantum"],
+        "url": "/quantum%20materials/data%20science/2026/01/20/CoSn/",
+        "teaser": "/assets/images/ToC_JACS.png"
+      },{
+        "title": "Topology and Disorder, Friends or Foes?",
+        "excerpt":"Introduction  Topological properties rely on the symmetry of electronic and magnetic degrees of freedom.   References   ","categories": ["Quantum Materials","Data Science"],
+        "tags": ["topology","quantum"],
+        "url": "/quantum%20materials/data%20science/2026/02/16/mn3sn/",
+        "teaser": "/assets/images/my-teaser-image.jpg"
+      }]
---
+++ blog/index.md
@@ -0,0 +1,16 @@
+---
+title: "Research Notes & Blog"
+layout: home
+permalink: /blog/
+author_profile: true
+entries_layout: list
+header:
+  overlay_image: /assets/images/header-blog.jpg  # Replace with your image path
+  overlay_filter: 0.5 # Darkens image by 50% so white text pops
+  caption: "Photo credit: [**Unsplash**](https://unsplash.com)"
+---
+
+# Welcome to my digital notebook
+Here I document my work in **condensed matter physics**, **data science**, and **scientific computing**. You will find tutorials on Python, RMCProfile, and notes from my daily research.
+
+---
